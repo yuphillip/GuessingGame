00a93a7370192a98cf4015f55d9cac77d5426e24
BT_1N3_02 <pyu6253@bths.edu>
2018-09-27 10:03:40 -0400

    testing ai

--- src/Pyu/PyuLib.java
+++ src/Pyu/PyuLib.java
@@ -3,7 +3,7 @@ package Pyu;
 import java.util.Scanner;
 
 public class PyuLib {
-    public static void main(String[]args)
+    public static void GuessingGame()
     {
         Scanner input = new Scanner(System.in);
         System.out.println("Can you guess my number ?");
@@ -21,5 +21,33 @@ public class PyuLib {
         input.close();
         System.out.println("Your guess was right!");
     }
+    public static void AiGuessingGame()
+    {
+        Scanner input = new Scanner(System.in);
+        int first = (int)(Math.random()*100);
+        System.out.println(first);
+        String response = (input.next()).toLowerCase();
+        while(!(response.equals("correct")))
+        {
+            if(response.equals("lower"))
+            {
+                int range = (first - 0)+1;
+                first = ((int)(Math.random()*range)) + 0;
+                System.out.println(first);
+            }
+            if(response.equals("higher"))
+            {
+                int range = (100 - first)+1;
+                first = ((int)(Math.random()*range)) + first;
+                System.out.println(first);
+            }
+            if(response.equals("correct"))
+            {
+                System.out.println("Yay! I won. Thanks for playing with me! :D");
+            }
+            response = (input.next()).toLowerCase();
+        }
+    }
 }
+
 
--- src/Pyu/testfile.java
+++ src/Pyu/testfile.java
@@ -1,4 +1,32 @@
 package Pyu;
 
+import java.util.Scanner;
+
+import static Pyu.PyuLib.AiGuessingGame;
+import static Pyu.PyuLib.GuessingGame;
+
 public class testfile {
+    public static void main(String[]args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Which game do you version do you want to play? You guessing? or Ai guessing?");
+        String response = input.nextLine();
+        response = response.toLowerCase();
+        if(response.equals("you guessing"))
+        {
+            GuessingGame();
+            input.close();
+        }
+        if (response.equals("ai guessing"))
+        {
+            AiGuessingGame();
+            input.close();
+        }
+        else
+        {
+            System.out.println("Sorry never heard of that before.");
+        }
+
+    }
 }
+
